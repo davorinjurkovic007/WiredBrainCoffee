5b094fae3278e52524714563b827418e7d8846ba
Davorin <davorin.jurkovic@gmail.com>
2021-03-08 22:50:23 +0100

    Mastering XAML Layouts

--- WiredBrainCoffee/WiredBrainCoffee.CustomerApp/MainPage.xaml.cs
+++ WiredBrainCoffee/WiredBrainCoffee.CustomerApp/MainPage.xaml.cs
@@ -20,5 +20,23 @@ namespace WiredBrainCoffee.CustomerApp
             var messageDialog = new MessageDialog("Custome added!");
             await messageDialog.ShowAsync();
         }
+
+        private void ButtonDeleteCustomer_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonMove_Click(object sender, RoutedEventArgs e)
+        {
+            //int column = (int)customerListGrid.GetValue(Grid.ColumnProperty);
+            int column = Grid.GetColumn(customerListGrid);
+
+            int newColumn = column == 0 ? 2 : 0;
+
+            //customerListGrid.SetValue(Grid.ColumnProperty, newColumn);
+            Grid.SetColumn(customerListGrid, newColumn);
+
+            moveSymbolIcon.Symbol = newColumn == 0 ? Symbol.Forward : Symbol.Back;
+        }
     }
 }
