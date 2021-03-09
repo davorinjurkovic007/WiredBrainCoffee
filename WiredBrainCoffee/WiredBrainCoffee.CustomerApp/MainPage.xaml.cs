c16b4714a2ed3d11098afd8953f213bdc6bda6c5
Davorin <davorin.jurkovic@gmail.com>
2021-03-09 23:38:11 +0100

    Accessing Objects from Code

--- WiredBrainCoffee/WiredBrainCoffee.CustomerApp/MainPage.xaml.cs
+++ WiredBrainCoffee/WiredBrainCoffee.CustomerApp/MainPage.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using WiredBrainCoffee.CustomerApp.Model;
+using WiredBrainCoffee.CustomersApp.DataProvider;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -10,20 +13,49 @@ namespace WiredBrainCoffee.CustomerApp
 {
     public sealed partial class MainPage : Page
     {
+        private CustomerDataProvider _customerDataProvider;
+
         public MainPage()
         {
             this.InitializeComponent();
+            this.Loaded += MainPage_Loaded;
+            App.Current.Suspending += App_Suspending;
+            _customerDataProvider = new CustomerDataProvider();
         }
 
-        private async void ButtonAddCustomer_Click(object sender, RoutedEventArgs e)
+        private async void MainPage_Loaded(object sender, RoutedEventArgs e)
         {
-            var messageDialog = new MessageDialog("Custome added!");
-            await messageDialog.ShowAsync();
+            customerListView.Items.Clear();
+
+            var customers = await _customerDataProvider.LoadCustomersAsync();
+
+            foreach(var customer in customers)
+            {
+                customerListView.Items.Add(customer);
+            }
+        }
+
+        private async void App_Suspending(object sender, Windows.ApplicationModel.SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            await _customerDataProvider.SaveCustomersAsync(customerListView.Items.OfType<Customer>());
+            deferral.Complete();
+        }
+
+        private void ButtonAddCustomer_Click(object sender, RoutedEventArgs e)
+        {
+            var customer = new Customer { FirstName = "New" };
+            customerListView.Items.Add(customer);
+            customerListView.SelectedItem = customer;
         }
 
         private void ButtonDeleteCustomer_Click(object sender, RoutedEventArgs e)
         {
-
+            var customer = customerListView.SelectedItem as Customer;
+            if(customer != null)
+            {
+                customerListView.Items.Remove(customer);
+            }
         }
 
         private void ButtonMove_Click(object sender, RoutedEventArgs e)
@@ -37,6 +69,35 @@ namespace WiredBrainCoffee.CustomerApp
             Grid.SetColumn(customerListGrid, newColumn);
 
             moveSymbolIcon.Symbol = newColumn == 0 ? Symbol.Forward : Symbol.Back;
+        }
+
+        private void customerListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var customer = customerListView.SelectedItem as Customer;
+            txtFirstName.Text = customer?.FirstName ?? "";
+            txtLastName.Text = customer?.LastName ?? "";
+            chkIsDeveloper.IsChecked = customer?.IsDeveloper;
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateCustomer();
+        }
+
+        private void CheckBox_IsCheckedChanged(object sender, RoutedEventArgs e)
+        {
+            UpdateCustomer();
+        }
+
+        private void UpdateCustomer()
+        {
+            var customer = customerListView.SelectedItem as Customer;
+            if(customer != null)
+            {
+                customer.FirstName = txtFirstName.Text;
+                customer.LastName = txtLastName.Text;
+                customer.IsDeveloper = chkIsDeveloper.IsChecked.GetValueOrDefault();
+            }
         }
     }
 }
---
+++ WiredBrainCoffee/WiredBrainCoffee.CustomerApp/Model/Customer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using WiredBrainCoffee.CustomerApp.Model.Base;
+
+namespace WiredBrainCoffee.CustomerApp.Model
+{
+    public class Customer : Observable
+    {
+        private string firstName;
+        private string lastName;
+        private bool isDeveloper;
+
+        public string FirstName
+        {
+            get => firstName;
+            set
+            {
+                firstName = value;
+                OnPropertyChanged();
+            }
+        }
+        public string LastName
+        {
+            get => lastName;
+            set
+            {
+                lastName = value;
+                OnPropertyChanged();
+            }
+        }
+        public bool IsDeveloper
+        {
+            get => isDeveloper;
+            set
+            {
+                isDeveloper = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
